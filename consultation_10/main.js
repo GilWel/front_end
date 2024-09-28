f1e381cbff31922258c676db9ea397bc6375f4f4
GilWel <gilnik2608@gmail.com>
2024-09-28 20:22:29 +0200

    Homework09_01

---
+++ consultation_10/main.js
@@ -0,0 +1,12 @@
+const arrOfNumbers = [2,4,5,10,15];
+const arrOfNumbers3 = [];
+
+for (let index = 0; index < arrOfNumbers.length; index++) {
+
+      arrOfNumbers3.push(Math.pow(arrOfNumbers[index], 2));
+    
+}
+ 
+
+console.log(arrOfNumbers3)
+
--- lesson_09/Homework09/example_01_2/main.js
+++ lesson_09/Homework09/example_01_2/main.js
@@ -8,6 +8,7 @@ for (i = 0; i < 10; i++) {
    console.log(arrayA);
 
 arrayA.sort((a, b) => a - b);
+
 console.log('Min: ' + arrayA[0]);
 console.log('Max: ' + arrayA[arrayA.length - 1]);
 
