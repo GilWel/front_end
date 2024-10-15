0f2c77edc92952ef2a88c799f989b7b2cc25d123
GilWel <gilnik2608@gmail.com>
2024-10-15 11:17:50 +0200

    Homework_11_4*

--- lesson_11/Homework_11/example_04/main1.js
+++ lesson_11/Homework_11/example_04/main1.js
@@ -1,18 +1,28 @@
 function filter(objects) {
 
-    const result = {};
+    let result = {};
+    let result1 = {};
 
-
-    // const valuesOfFilter = Object.values(objects);
-    // const keysOfFilter = Object.keys(objects);
     const OfFilter = Object.entries(objects);
 
     OfFilter.forEach(element => {
-result[element[1]] = "value: " + element[1] + ", type: " + (typeof element[0]);
-      
+        result[element[1]] = element[1] + ", type: " + (typeof element[0]);
+
+        const value = result[element[1]];
+
+        result1[element[0]] = {value};
+       
+
+
     });
- 
-    return result;
+
+    return result1;
+
+
+
+
+
+
 };
 
 
